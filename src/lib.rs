ae2cab9def9b29e24e2055bfe4988fd88e1894ca
Vincent Sparks <vince@spacestation.blue>
2024-09-15 23:07:06 -0700

    added PendingMap

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,26 @@ pub enum Pending<T> {
     Failed,
 }
 
+pub enum PendingMap<T,U,F> {
+    Ready(U),
+    Pending(oneshot::Receiver<T>, F),
+    Failed,
+    Panicked,
+}
+
 impl<T> Pending<T> {
     pub fn try_load(&mut self) -> Option<&mut T> {
-        replace_with::replace_with_or_abort(self, |me| {
-            match me {
-                Pending::Pending(rcvr) => match rcvr.try_recv() {
-                    Ok(val) => Pending::Ready(val),
-                    Err(oneshot::TryRecvError::Empty) => Pending::Pending(rcvr),
-                    Err(oneshot::TryRecvError::Disconnected) => Pending::Failed,
-                }
-                other => other,
-            }
-        });
+        if let Pending::Pending(rcvr) = self {
+            match rcvr.try_recv() {
+                Ok(val) => {
+                    *self = Pending::Ready(val);
+                },
+                Err(oneshot::TryRecvError::Empty) => {},
+                Err(oneshot::TryRecvError::Disconnected) => {
+                    *self = Pending::Failed;
+                },
+            };
+        }
         match self {
             Pending::Ready(val) => Some(val),
             _ => None,
@@ -29,5 +37,33 @@ impl<T> Pending<T> {
     pub fn new() -> (oneshot::Sender<T>, Self) {
         let (sender, receiver) = oneshot::channel();
         (sender, Self::Pending(receiver))
+    }
+}
+
+impl<T,U,F: FnOnce(T)->U> PendingMap<T,U,F> {
+    pub fn try_load(&mut self) -> Option<&mut U> {
+        if let PendingMap::Pending(rcvr, _) = self {
+            match rcvr.try_recv() {
+                Ok(val) => {
+                    let PendingMap::Pending(_, func) = std::mem::replace(self, PendingMap::Panicked) else {unreachable!()};
+                    *self = PendingMap::Ready(func(val));
+                },
+                Err(oneshot::TryRecvError::Empty) => {},
+                Err(oneshot::TryRecvError::Disconnected) => {*self = PendingMap::Failed},
+            }
+        }
+        match self {
+            PendingMap::Ready(val) => Some(val),
+            _ => None,
+        }
+    }
+
+    pub fn is_failed(&self) -> bool {
+        matches!(self, PendingMap::Failed)
+    }
+
+    pub fn new(func: F) -> (oneshot::Sender<T>, Self) {
+        let (sender, receiver) = oneshot::channel();
+        (sender, Self::Pending(receiver, func))
     }
 }
